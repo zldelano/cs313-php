99aa41cdb30013f147c8a424c2d11268fdcf0781
zldelano <del15002@byui.edu>
2019-05-03 20:49:04 -0600

    start following tutorial: https://www.youtube.com/watch?time_continue=3&v=FEmysQARWFU

---
+++ web/home/about_me.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <link href="https://fonts.googleapis.com/css?family=Work+Sans:300">
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Document</title>
+   <link rel="stylesheet" href="style.css">
+</head>
+<body>
+   <?php
+    require 'nav.php';
+   ?>
+   <h1>About Me</h1>
+
+</body>
+</html>
---
+++ web/home/nav.php
@@ -0,0 +1,13 @@
+<header>
+    <div class="container">
+        <img src="img/logo_black.png" alt="logo" class="logo">
+        <nav>
+            <ul>
+                <?php
+                    echo "<li><a href='./about_me.php'>About Me</a></li>";
+                    echo "<li><a href='./assignmentshp'>Assignments</a></li>";
+                ?>
+            </ul>
+        </nav>
+    </div>
+</header>
