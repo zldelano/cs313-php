b8f1401a0d11b38af32d226b674317a90ecfce88
zldelano <del15002@byui.edu>
2019-05-04 19:53:05 -0600

    adjust height of logo

--- web/home/nav.php
+++ web/home/nav.php
@@ -1,6 +1,6 @@
 <header>
     <div class="container">
-        <img src="img/logo_black.png" alt="logo" class="logo" width="200px" height="100px">
+        <img src="img/logo_black.png" alt="logo" class="logo" width="200px" height="50px">
         <nav>
             <ul>
                 <?php
