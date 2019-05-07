b115f43a9171710ef7857277d5afcd26128fb19e
zldelano <del15002@byui.edu>
2019-05-07 15:37:35 -0600

    line breaks

--- web/team/week03/index.php
+++ web/team/week03/index.php
@@ -5,16 +5,16 @@
       <div class="content">
          <form action="processform.php" id="the_form" method="POST">
             <label>Name: </label>
-            <input type="text" name="name" size="25"/>
+            <input type="text" name="name" size="25"/><br>
             <label>Email: </label>
-            <input type="text" name="email" size="25"/>
-            <label>Major: </label>
+            <input type="text" name="email" size="25"/><br>
+            <label>Major: </label><br>
                <input type="radio" name="major" value="cs">Computer Science<br>            
                <input type="radio" name="major" value="wdd">Web Design & Development<br>            
                <input type="radio" name="major" value="cit">Computer Information Technology<br>
                <input type="radio" name="major" value="ce">Computer Engineering<br>
             <label>Comments: </label>
-            <textarea name="comment" id="comments">Enter text here...</textarea>
+            <textarea name="comment" id="comments">Enter text here...</textarea><br><br>
             <input type="submit" name="submit" value="Send" id="submit">
          </form>
       </div>
