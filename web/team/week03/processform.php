3bb6ec85677425df10dc88f486d6b50c8f74b259
zldelano <del15002@byui.edu>
2019-05-07 15:51:27 -0600

    team 03 loop over continents

--- web/team/week03/processform.php
+++ web/team/week03/processform.php
@@ -3,9 +3,13 @@ $name = $_POST["name"];
 $email = $_POST["email"];
 $major = $_POST["major"];
 $comments = $_POST["comments"];
+$continents = $_POST["continents"];
 
 echo "$name <br>";
 echo "<a href=\"mailto:$email\">$email</a><br>";
 echo "$major<br>";
 echo "$comments<br>";
+foreach ($continents as $continent) {
+   echo "$continent<br>";
+}
 ?>
