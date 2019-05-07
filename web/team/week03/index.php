b256f4e4222ae41485a7086443130fa0fec8373e
zldelano <del15002@byui.edu>
2019-05-07 15:36:08 -0600

    team03

---
+++ web/team/week03/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+   <head></head>
+   <body>
+      <div class="content">
+         <form action="processform.php" id="the_form" method="POST">
+            <label>Name: </label>
+            <input type="text" name="name" size="25"/>
+            <label>Email: </label>
+            <input type="text" name="email" size="25"/>
+            <label>Major: </label>
+               <input type="radio" name="major" value="cs">Computer Science<br>            
+               <input type="radio" name="major" value="wdd">Web Design & Development<br>            
+               <input type="radio" name="major" value="cit">Computer Information Technology<br>
+               <input type="radio" name="major" value="ce">Computer Engineering<br>
+            <label>Comments: </label>
+            <textarea name="comment" id="comments">Enter text here...</textarea>
+            <input type="submit" name="submit" value="Send" id="submit">
+         </form>
+      </div>
+   </body>
+</html>
