23b75d9af117b4e9f9ef6cbe8353bf3f9651559d
zldelano <del15002@byui.edu>
2019-05-09 15:53:12 -0600

    add beginning of week 03 assignment

---
+++ web/prove/03/nav.php
@@ -0,0 +1,13 @@
+<header>
+    <div class="container">
+        <img src="img/logo_black.png" alt="logo" class="logo" width="200px" height="50px">
+        <nav>
+            <ul>
+                <?php
+                    echo "<li><a href='./browse.php'>Browse</a></li>";
+                    echo "<li><a href='./shopping_cart.php'>Shopping Cart</a></li>";
+                ?>
+            </ul>
+        </nav>
+    </div>
+</header>
---
+++ web/prove/03/shopping_cart.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://fonts.googleapis.com/css?family=Work+Sans:400">
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Document</title>
+   <link rel="stylesheet" href="style.css">
+</head>
+<body>
+   <?php
+      require 'nav.php';
+   ?>
+   <h1>Shopping Cart</h1>
+   <p>Lol nothing is here boi.</p>
+ 
+</body>
+</html>
