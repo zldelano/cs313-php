1722f411739de23aaba29441cd75102a181a0345
zldelano <del15002@byui.edu>
2019-05-11 19:28:01 -0600

    increase image size and add buttons

--- web/prove/03/browse.php
+++ web/prove/03/browse.php
@@ -18,14 +18,33 @@
    ?>
    <div class="selection" id="selection">
       <h1>Browse</h1>
-      <img src="img/ties/87933513_650_main.jpg" id="tie1" height="100" width="100" alt="Pink and Blue Checkered">
-      <img src="img/ties/87933732_650_main.jpg" id="tie2" height="100" width="100" alt="Pink Floral">
+      <div class="imgButtonPair">
+         <img src="img/ties/87933513_650_main.jpg" id="tie1" height="200" width="200" alt="Pink and Blue Checkered">
+         <button type="button">Add to Cart</button>
+      </div>
+      <div class="imgButtonPair">
+         <img src="img/ties/87933732_650_main.jpg" id="tie2" height="200" width="200" alt="Pink Floral">
+         <button type="button">Add to Cart</button>
+      </div>
       <br>
-      <img src="img/ties/A7982311_680_main.jpg" id="tie3" height="100" width="100" alt="Dark Blue and Pink Floral">
-      <img src="img/ties/K7982238_602_main.jpg" id="tie4" height="100" width="100" alt="Purple">
+      <div class="imgButtonPair">
+         <img src="img/ties/A7982311_680_main.jpg" id="tie3" height="200" width="200" alt="Dark Blue and Pink Floral">
+         <button type="button">Add to Cart</button>
+      </div>
+      <div class="imgButtonPair">
+         <img src="img/ties/K7982238_602_main.jpg" id="tie4" height="200" width="200" alt="Purple">
+         <button type="button">Add to Cart</button>
+      </div>
       <br>
-      <img src="img/ties/K7982242_455_main.jpg" id="tie5" height="100" width="100" alt="Blue Vine/Floral">
-      <img src="img/ties/K7982242_500_main.jpg" id="tie6" height="100" width="100" alt="Purple Vine/Floral">
+      <div class="imgButtonPair">
+         <img src="img/ties/K7982242_455_main.jpg" id="tie5" height="200" width="200" alt="Blue Vine/Floral">
+         <button type="button">Add to Cart</button>
+      </div>
+      <div class="imgButtonPair">
+         <img src="img/ties/K7982242_500_main.jpg" id="tie6" height="200" width="200" alt="Purple Vine/Floral">
+         <button type="button">Add to Cart</button>
+      </div>
+      <br>
    </div>
 </body>
 </html>
