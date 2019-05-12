fdf8e4d9c86041329229568b029227454a4075ac
zldelano <del15002@byui.edu>
2019-05-11 19:15:14 -0600

    add tie imageS

--- web/prove/03/browse.php
+++ web/prove/03/browse.php
@@ -1,3 +1,7 @@
+<?php
+   session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,8 +18,14 @@
    ?>
    <h1>Browse</h1>
    <div class="selection" id="selection">
-      <!--  -->
+      <img src="img/ties/87933513_650_main.jpg" id="tie1" height="100" width="100" alt="Pink and Blue Checkered">
+      <img src="img/ties/87933732_650_main.jpg" id="tie2" height="100" width="100" alt="Pink Floral">
+      <br>
+      <img src="img/ties/A7982311_680_main.jpg" id="tie3" height="100" width="100" alt="Dark Blue and Pink Floral">
+      <img src="img/ties/K7982238_602_main.jpg" id="tie4" height="100" width="100" alt="Purple">
+      <br>
+      <img src="img/ties/K7982242_455_main.jpg" id="tie5" height="100" width="100" alt="Blue Vine/Floral">
+      <img src="img/ties/K7982242_500_main.jpg" id="tie6" height="100" width="100" alt="Purple Vine/Floral">
    </div>
- 
 </body>
 </html>
