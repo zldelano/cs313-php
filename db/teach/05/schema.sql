a64924a7af3c8e1cf850b7ab949a474c406e44ed
zldelano <del15002@byui.edu>
2019-05-21 16:00:30 -0600

    beginning of teach04 activity

---
+++ db/teach/05/schema.sql
@@ -0,0 +1,25 @@
+-- Setup extensions
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+-- Create tables
+DROP TABLE IF EXISTS teach04_scripture;
+CREATE TABLE teach04_scripture (
+   id       UUID           NOT NULL       DEFAULT uuid_generate_v4(),
+   book     VARCHAR(50)    NOT NULL,
+   chapter  NUMERIC(3,0)   NOT NULL,
+   verse    NUMERIC(3,0)   NOT NULL,
+   content  TEXT           NOT NULL,
+
+   -- key setup
+   PRIMARY KEY (id)
+);
+
+-- inserting into teach04_scripture
+INSERT INTO teach04_scripture
+   (book, chapter, verse, content)
+VALUES
+   ('John', 1, 5, 'And the light shineth in darkness; and the darkness comprehended it not.'),
+   ('Doctrine and Covenants', 88, 49, 'The light shineth in darkness, and the darkness comprehendeth it not; nevertheless, the day shall come when you shall comprehend even God, being quickened in him and by him.'),
+   ('Doctrine and Covenants', 93, 28, 'He that akeepeth his commandments receiveth btruth and clight, until he is glorified in truth and dknoweth all things.'),
+   ('Mosiah', 16, 9, 'He is the light and the life of the world; yea, a light that is endless, that can never be darkened; yea, and also a life which is endless, that there can be no more death.');
+
---
+++ web/team/week05/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://fonts.googleapis.com/css?family=Work+Sans:400">
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Document</title>
+   <link rel="stylesheet" href="style.css">
+</head>
+<body>
+   <h1>Scripture Resources</h1>
+   <?php
+      require 'nav.php';
+
+      try
+      {
+         $dbUrl = getenv('DATABASE_URL');
+
+         $dbOpts = parse_url($dbUrl);
+
+         $dbHost = $dbOpts["host"];
+         $dbPort = $dbOpts["port"];
+         $dbUser = $dbOpts["user"];
+         $dbPassword = $dbOpts["pass"];
+         $dbName = ltrim($dbOpts["path"],'/');
+
+         $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+         // this line makes PDO give us an exception when there are problems,
+         // and can be very helpful in debugging! (But you would likely want
+         // to disable it for production environments.)
+         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+         // queries
+         foreach ($db->query('SELECT book, chapter, verse, content FROM teach04_scripture') as $row)
+         {
+            echo '<b>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</b> - ' . $row['content'];
+            echo '<br/>';
+         }
+      }
+      catch (PDOException $ex)
+      {
+         echo 'Error!: ' . $ex->getMessage();
+         die();
+      }
+   ?>
+ 
+</body>
+</html>
