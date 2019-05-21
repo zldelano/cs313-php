49c5e390574da7470c092922199e029ca5a5a87c
zldelano <del15002@byui.edu>
2019-05-21 16:05:11 -0600

    comment out stuff in nav

--- db/teach/05/schema.sql
+++ db/teach/05/schema.sql
@@ -20,6 +20,6 @@ INSERT INTO teach04_scripture
 VALUES
    ('John', 1, 5, 'And the light shineth in darkness; and the darkness comprehended it not.'),
    ('Doctrine and Covenants', 88, 49, 'The light shineth in darkness, and the darkness comprehendeth it not; nevertheless, the day shall come when you shall comprehend even God, being quickened in him and by him.'),
-   ('Doctrine and Covenants', 93, 28, 'He that akeepeth his commandments receiveth btruth and clight, until he is glorified in truth and dknoweth all things.'),
+   ('Doctrine and Covenants', 93, 28, 'He that keepeth his commandments receiveth btruth and clight, until he is glorified in truth and dknoweth all things.'),
    ('Mosiah', 16, 9, 'He is the light and the life of the world; yea, a light that is endless, that can never be darkened; yea, and also a life which is endless, that there can be no more death.');
 
--- web/team/week05/nav.php
+++ web/team/week05/nav.php
@@ -1,11 +1,11 @@
 <header>
     <div class="container">
-        <img src="img/logo_black.png" alt="logo" class="logo" width="200px" height="50px">
+        <!-- <img src="img/logo_black.png" alt="logo" class="logo" width="200px" height="50px"> -->
         <nav>
             <ul>
                 <?php
-                    echo "<li><a href='./about_me.php'>About Me</a></li>";
-                    echo "<li><a href='./assignments.php'>Assignments</a></li>";
+                    // echo "<li><a href='./about_me.php'>About Me</a></li>";
+                    // echo "<li><a href='./assignments.php'>Assignments</a></li>";
                 ?>
             </ul>
         </nav>
