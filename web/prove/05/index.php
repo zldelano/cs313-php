d1f72cbbb85c28a673b8ac02c05ec709912c528f
zldelano <del15002@byui.edu>
2019-05-25 22:14:38 -0600

    HAHA. PRICE DOES NOT EXIST

--- web/prove/05/index.php
+++ web/prove/05/index.php
@@ -21,7 +21,7 @@
       $service_job_info_rows = null;
       try {
          // get the list of jobs the advisor can select from
-         $stmt = $db->prepare('SELECT job_name, price FROM service_job_info');
+         $stmt = $db->prepare('SELECT job_name, cost FROM service_job_info');
          $stmt->execute();
          $service_job_info_rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
       }
@@ -38,16 +38,16 @@
       
       $ji_length=sizeof($service_job_info_rows);
       echo "the length of the query is $ji_length<br>";
-      // for ($i = 1; i <= $ji_length; $i++) {
-      // //    echo "<select name=$job_name_field>";
-      // //    foreach ($service_job_info_rows as $ji)
-      // //    {
-      // //       $option_value=$ji['job_name'];
-      // //       echo "<option value=$i>$option_value</option>";
-      // //    }
-      // //    echo "</select>";
-      // echo "test";
-      // }
+      for ($i = 1; i <= $ji_length; $i++) {
+         echo "<select name=$job_name_field>";
+         foreach ($service_job_info_rows as $ji)
+         {
+            $option_value=$ji['job_name'];
+            echo "<option value=$i>$option_value</option>";
+         }
+         echo "</select>";
+      echo "test";
+      }
       // echo "Job #1:         <input type=\"text\" name=$job_name_field><br>";
       // echo "Job #1 Tech:    <input type=\"text\" name=$tech_name_field><br>";
       echo "</form>"
