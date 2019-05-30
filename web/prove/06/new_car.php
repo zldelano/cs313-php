ccb8a365d55853a27c7a5f7205da4a3fe145e219
zldelano <del15002@byui.edu>
2019-05-30 16:10:41 -0600

    it would be helpful to actually have the table tag

--- web/prove/06/new_car.php
+++ web/prove/06/new_car.php
@@ -17,24 +17,26 @@
    ?>
    <h1>New Car</h1>
    <form action="new_car.php" method="post">
-      <tr>
-      <td>VIN:</td>           <td><input type="text" name="newcar_vin"   required><br></td>
-      </tr>
-      <tr>
-      <td>Color:</td>         <td><input type="text" name="newcar_color" required><br></td>
-      </tr>
-      <tr>
-      <td>Make:</td>          <td><input type="text" name="newcar_make"  required><br></td>
-      </tr>
-      <tr>
-      <td>Model:</td>         <td><input type="text" name="newcar_model" required><br></td>
-      </tr>
-      <tr>
-      <td>Year:</td>          <td><input type="text" name="newcar_year"  required><br></td>
-      </tr>
-      <tr>
-      <td>Owner email:</td>   <td><input type="text" name="newcar_owner" required><br></td>
-      </tr>
+      <table>
+         <tr>
+         <td>VIN:</td>           <td><input type="text" name="newcar_vin"   required><br></td>
+         </tr>
+         <tr>
+         <td>Color:</td>         <td><input type="text" name="newcar_color" required><br></td>
+         </tr>
+         <tr>
+         <td>Make:</td>          <td><input type="text" name="newcar_make"  required><br></td>
+         </tr>
+         <tr>
+         <td>Model:</td>         <td><input type="text" name="newcar_model" required><br></td>
+         </tr>
+         <tr>
+         <td>Year:</td>          <td><input type="text" name="newcar_year"  required><br></td>
+         </tr>
+         <tr>
+         <td>Owner email:</td>   <td><input type="text" name="newcar_owner" required><br></td>
+         </tr>
+      </table>
       <input type="submit"><br>
    </form>
 </body>
