ecf9c2341b465728c3600c1778d1fa76b71f1e75
zldelano <del15002@byui.edu>
2019-05-30 16:09:21 -0600

    form in table form

--- web/prove/06/new_car.php
+++ web/prove/06/new_car.php
@@ -17,12 +17,24 @@
    ?>
    <h1>New Car</h1>
    <form action="new_car.php" method="post">
-      VIN:           <input type="text" name="newcar_vin"><br>
-      Color:         <input type="text" name="newcar_color"><br>
-      Make:          <input type="text" name="newcar_make"><br>
-      Model:         <input type="text" name="newcar_model"><br>
-      Year:          <input type="text" name="newcar_year"><br>
-      Owner email:   <input type="text" name="newcar_owner"><br>
+      <tr>
+      <td>VIN:</td>           <td><input type="text" name="newcar_vin"   required><br></td>
+      </tr>
+      <tr>
+      <td>Color:</td>         <td><input type="text" name="newcar_color" required><br></td>
+      </tr>
+      <tr>
+      <td>Make:</td>          <td><input type="text" name="newcar_make"  required><br></td>
+      </tr>
+      <tr>
+      <td>Model:</td>         <td><input type="text" name="newcar_model" required><br></td>
+      </tr>
+      <tr>
+      <td>Year:</td>          <td><input type="text" name="newcar_year"  required><br></td>
+      </tr>
+      <tr>
+      <td>Owner email:</td>   <td><input type="text" name="newcar_owner" required><br></td>
+      </tr>
       <input type="submit"><br>
    </form>
 </body>
