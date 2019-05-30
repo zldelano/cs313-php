3a8a8456033394b8517404880c116bc6bda69697
zldelano <del15002@byui.edu>
2019-05-30 16:28:06 -0600

    add insert ability for vehicles

--- web/prove/06/new_car.php
+++ web/prove/06/new_car.php
@@ -11,30 +11,54 @@
 <body>
    <?php
       require 'nav.php';
-      require 'db_connect.php';
+      
+      if (len($_POST) > 0)
+      {
+         try
+         {
+            // only connect if the user submitted a form
+            require 'db_connect.php';
 
-      echo var_dump($_POST);
+            $newcar_stmt = $db->prepare("INSERT INTO service_vehicle
+                                       (vin, color, make, model, year, owner)
+                                       VALUES (:vin, :color, :make, :model, :year, :owner)");
+            $newscrip_stmt->bindParam(':vin', htmlspecialchars($_POST['newcar_vin']));
+            $newscrip_stmt->bindParam(':color', htmlspecialchars($_POST['newcar_color']));
+            $newscrip_stmt->bindParam(':make', htmlspecialchars($_POST['newcar_make']));
+            $newscrip_stmt->bindParam(':model', htmlspecialchars($_POST['newcar_model']));
+            $newscrip_stmt->bindParam(':year', htmlspecialchars($_POST['newcar_year']));
+            $newscrip_stmt->bindParam(':owner', htmlspecialchars($_POST['newcar_owner']));
+            $newscrip_stmt->execute();
+            echo "New vehicle successfully added!";
+         }
+         catch (PDOException $ex)
+         {
+            // GET RID OF THIS AT SOME POINT--DON'T GIVE UP THE WHOLE SHOP
+            echo 'Error!: ' . $ex->getMessage();
+            die();
+         }
+      }
    ?>
    <h1>New Car</h1>
    <form action="new_car.php" method="post">
       <table>
          <tr>
-         <td>VIN:</td>           <td><input type="text" name="newcar_vin"   required><br></td>
+            <td>VIN:</td>         <td><input type="text" name="newcar_vin"   required><br></td>
          </tr>
          <tr>
-         <td>Color:</td>         <td><input type="text" name="newcar_color" required><br></td>
+            <td>Color:</td>       <td><input type="text" name="newcar_color" required><br></td>
          </tr>
          <tr>
-         <td>Make:</td>          <td><input type="text" name="newcar_make"  required><br></td>
+            <td>Make:</td>        <td><input type="text" name="newcar_make"  required><br></td>
          </tr>
          <tr>
-         <td>Model:</td>         <td><input type="text" name="newcar_model" required><br></td>
+            <td>Model:</td>       <td><input type="text" name="newcar_model" required><br></td>
          </tr>
          <tr>
-         <td>Year:</td>          <td><input type="text" name="newcar_year"  required><br></td>
+            <td>Year:</td>        <td><input type="text" name="newcar_year"  required><br></td>
          </tr>
          <tr>
-         <td>Owner email:</td>   <td><input type="text" name="newcar_owner" required><br></td>
+            <td>Owner email:</td> <td><input type="text" name="newcar_owner" required><br></td>
          </tr>
       </table>
       <input type="submit"><br>
