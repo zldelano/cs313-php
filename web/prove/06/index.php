a5851007b1d8a207471272f60c4c73ab2183ecbf
zldelano <del15002@byui.edu>
2019-06-01 17:51:38 -0600

    is this the error?

--- web/prove/06/index.php
+++ web/prove/06/index.php
@@ -41,9 +41,9 @@
          $ns_notes = $_POST['new_service_notes'];
          $ns_tech  = $_POST['new_service_tech'];
 
-         // // set up the statement
-         // $stmt = $db->prepare('INSERT INTO service_service
-         //                       VALUES (:id, :vin, :email, :notes, :tech)');
+         // set up the statement
+         $stmt = $db->prepare('INSERT INTO service_service
+                               VALUES (:id, :vin, :email, :notes, :tech)');
          // $stmt->bindParam(':id', htmlspecialchars($ns_id));
          // $stmt->bindParam(':vin', htmlspecialchars($ns_vin));
          // $stmt->bindParam(':email', htmlspecialchars($ns_email));
