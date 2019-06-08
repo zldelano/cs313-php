5d0373f89764179eef5310f32d6a26b14f41476a
zldelano <del15002@byui.edu>
2019-06-08 12:16:22 -0600

    I told the customer enough

--- web/prove/07/new_car.php
+++ web/prove/07/new_car.php
@@ -6,8 +6,6 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <meta http-equiv="X-UA-Compatible" content="ie=edge">
    <title>Document</title>
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-   <script src="script.js"></script>
    <link rel="stylesheet" href="style.css">
 </head>
 <body>
--- web/prove/07/script.js
+++
@@ -1,5 +0,0 @@
-$(".vin").keypress(function(e) {
-    if (($this).val().length() != 17) {
-        ($this).after('<span class="error">Must have 17 characters');
-    }
-});
